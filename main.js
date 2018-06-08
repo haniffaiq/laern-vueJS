68c3f3ad50d7b10d6110afdfc7a16b177feaf9f5
haniffaiq <haniffaiq95@gmail.com>
2018-06-08 16:07:04 +0700

    Add computed Properties

--- main.js
+++ main.js
@@ -4,15 +4,44 @@ new Vue({
         greet:"Welcome to Vuejs Learn page",      
         name, 
         x: 0,
-        y: 0
+        y: 0,
+        age : 17,
+        setName:"",
+        a : 0,
+        b : 0
     },
     methods:{
         submit: function(event){
-            console.log(this.name)
-            console.log(event)
-
+            //console.log(this.setName)
+            //console.log(event)
+            this.name = this.setName
             this.x = event.x
             this.y = event.y
+        },
+        add(value){
+            this.age = this.age + value
+            // that's code same like //this,age = this.age + 1
+        },
+        substract(value){
+            this.age = this.age - value
+        },
+        /*addToA(){
+            console.log("ageToA")
+            return this.age + this.a
+            },
+        addToB() {
+            console.log("ageToB")
+            return this.age + this.b
+            }*/
+    },
+    computed: {
+        addToA() {
+            console.log("ageToA")
+            return this.age + this.a
+        },
+        addToB() {
+            console.log("ageToB")
+            return this.age + this.b
         }
     }
 })
